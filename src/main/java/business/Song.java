3d88f6b2c38b617bf8844a3e16a19d70f41ed67c
Cal Woods <D00270955@student.dkit.ie>
2025-02-25 18:31:06 +0000

    Feature: Added constructors to 'Song'.
    Added two constructors: a no-argument that sets Song attributes artist to "Joe Bloggs" & title to "Mmmbop".
    
    The other constructor took two arguments: artist, title and set class attributes respectively.

--- src/main/java/business/Song.java
+++ src/main/java/business/Song.java
@@ -9,5 +9,22 @@ public class Song {
     private String artist;
     private String title;
 
-    
+    //constructor - no argument
+    /**
+     * 
+     */
+    public Song() {
+        //Set class attributes to defaults "Joe Bloggs", "Mmmbop" respectively
+        this.artist = "Joe Bloggs";
+        this.title = "Mmmbop";
+    }
+
+    public Song(String artist, String title) {
+        //Validation
+        if(artist.isBlank() || title.isBlank()) throw new IllegalArgumentException("Given artist/title must NOT be empty or contain only spaces!");
+        
+        //Set attributes to arguments
+        this.artist = artist;
+        this.title = title;
+    }
 }
