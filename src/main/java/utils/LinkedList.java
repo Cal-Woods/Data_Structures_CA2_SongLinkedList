62dfc4631f79065c6b1fbc080baaa1cb8a6328ad
Cal Woods <D00270955@student.dkit.ie>
2025-02-27 21:01:32 +0000

    Feature: Added constructor for LinkedList.
    Added a no-argument LinkedList constructor that initialises 'first', 'last' to null and initialises 'numElements' to 0 to imitate an empty list.
    
    Additionally, added a new 'Node' attribute: 'data' to store a 'Song' object.

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -17,12 +17,23 @@ public class LinkedList {
     //Track last Node in my LinkedList
     private Node last;
     
+    //Constructors
+    
+    /**
+     * A constructor that sets instance to default empty LinkedList.
+     */
+    public LinkedList() {
+        //Set attributes
+        this.first = null;
+        this.last = null;
+        this.numElements = 0;
+    }
+
     //Declare private static subclass 'Node' to contain reference to next Node and reference to prev Node
     private static class Node {
         //Attributes of 'Node'
         private Node prev;
         private Node next;
-
-        
+        private Song data;
     }
 }
