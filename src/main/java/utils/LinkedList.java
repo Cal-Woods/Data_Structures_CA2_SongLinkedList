2daeab8255df7103478c186913dba00ccb945282
Cal Woods <D00270955@student.dkit.ie>
2025-02-27 19:34:29 +0000

    Feature: Added two attributes & a private static class 'Node'.
    added 'numElements' for tracking number of elements in my LinkedList class, added 'first' to track first Node object in LinkedList, added 'last' to track final Node object in LinkedList and finally, started a new subclass in LinkedList called 'Node' which is marked 'private static'.

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -7,5 +7,18 @@ import business.Song;
  * @author michelle contributed by cal woods
  */
 public class LinkedList {
+    //Attributes
+    //Tracker for numElements
+    private int numElements;
     
+    //Track first Node in my LinkedList
+    private Node first;
+
+    //Track last Node in my LinkedList
+    private Node last;
+    
+    //Declare private static subclass 'Node' to contain reference to next Node and reference to prev Node
+    private static class Node {
+        
+    }
 }
