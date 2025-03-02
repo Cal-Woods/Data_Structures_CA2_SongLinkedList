b10a3070b07d2420049a8e4c3c6cceadf3483c38
Cal Woods <D00270955@student.dkit.ie>
2025-03-02 17:49:56 +0000

    Test: Added test method to check that a Song object was added to blank list in correct place.

--- src/test/java/utils/LinkedListExtraTests.java
+++ src/test/java/utils/LinkedListExtraTests.java
@@ -2,6 +2,8 @@ package utils;
 
 import org.junit.jupiter.api.Test;
 
+import business.Song;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -11,5 +13,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LinkedListExtraTests {
 
     public LinkedListExtraTests() {
+    }
+
+    //Cal's code from here
+
+    /**
+     * Tests adding one element to a list is at the correct index
+     */
+    @Test
+    public void testAddOneValidElementToBlankListIsThereWithGetPos0() {
+        LinkedList list = new LinkedList();
+
+        list.add(new Song("Eminem", "Lose Yourself"));
+
+        Song expected = new Song("Eminem", "Lose Yourself");
+        Song actual = list.get(0);
+
+        assertEquals(expected, actual);
     }
 }
