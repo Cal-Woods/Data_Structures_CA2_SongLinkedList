6a0b2946ddd3f12a27490904af8a7a481a8fb8b4
Cal Woods <D00270955@student.dkit.ie>
2025-03-02 18:31:17 +0000

    Docs: Edited javadocs for 'add()' method.

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -68,9 +68,8 @@ public class LinkedList {
      * Adds a new element to end of instance list.
      * @param data A new Song object
      * 
-     * 
-     * 
-     * @throws IllegalArgumentException by internal class If Song object is null.
+     * @throws IllegalArgumentException by Song class If Song object is null.
+     * @throws IllegalArgumentException If a duplicate Song object was found in instance.
      */
     public void add(Song data) {
         //Validation - Check if data is duplicate to other values, using private static, internal method 'isDuplicate(LinkedList list, Song data)'
